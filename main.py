77cc9fe8e9d9b3c23c6c6ff68f6dd0095423da77
Davi <davimoreira58@gmail.com>
2024-07-26 12:12:27 -0300

    configurando imports

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 import os
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
 source = os.path.abspath('.')
 path = os.path.join(source, 'db')
