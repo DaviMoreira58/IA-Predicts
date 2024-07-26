c8721ac72c09e06cd64846cbf2e550cf93ca49d4
Davi <davimoreira58@gmail.com>
2024-07-26 12:08:28 -0300

    encoder profissao

--- main.py
+++ main.py
@@ -7,4 +7,6 @@ path = os.path.join(source, 'db')
 db_clt = os.path.join(path, 'clientes.csv')
 
 table = pd.read_csv(db_clt)
-print(table.info())
+
+encoder = LabelEncoder()
+table['profissao'] = encoder.fit_transform(table['profissao'])
