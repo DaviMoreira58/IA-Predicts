1cd5243547757408cd22d2742a0d4a16eda3b8d7
Davi <davimoreira58@gmail.com>
2024-07-26 12:00:36 -0300

    criando e verificando os caminhos

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 import os
+
+source = os.path.abspath('.')
+path = os.path.join(source, 'db')
+db_clt = os.path.join(path, 'clientes.csv')
+
+print(os.path.exists(db_clt))
